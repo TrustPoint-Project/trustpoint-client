41349d6a17e7787efb008c6f895dad1bd192f695
Christian Schwinne <cschwinne@gmail.com>
2024-01-15 09:57:03 +0100

    Step 1: Download Trust Store

--- trustpoint_client/cli.py
+++ trustpoint_client/cli.py
@@ -1,26 +1,82 @@
 import click
 from trustpoint_client.trustpoint_client import provision as _provision
 
+version = "0.1.0"
+
+asciiLogo : str = """\b
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@
+@@@@     @@ @   @@ @@@ @@@   @@@     @@@@
+@@@@@ @@@@@  @@@@@ @@@ @@ @@@ @@@ @@@@@@@
+@@@@@ @@@@@ @@@@@@ @@@ @@@  @@@@@ @@@@@@@
+@@@@@ @@ @@ @@@@@@ @@  @@@@@  @@@ @@ @@@@
+@@@@@@  @@@ @@@@@@@  @ @@    @@@@@  @@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+    
+                    @            
+                                 @
+    @ @@    @@@   @@@    @ @@   @@@@@
+    @@  @  @   @    @    @@  @   @
+    @   @  @   @    @    @   @   @  @
+    @@@@    @@@   @@@@@  @   @    @@
+    @
+    @
+"""
+
+def drawAsciiLogo() -> None:
+    click.echo("""\b
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@
+    @@@@     @@ @   @@ @@@ @@@   @@@     @@@@
+    @@@@@ @@@@@  @@@@@ @@@ @@ @@@ @@@ @@@@@@@
+    @@@@@ @@@@@ @@@@@@ @@@ @@@  @@@@@ @@@@@@@
+    @@@@@ @@ @@ @@@@@@ @@  @@@@@  @@@ @@ @@@@
+    @@@@@@  @@@ @@@@@@@  @ @@    @@@@@  @@@@@
+    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+     
+                        @            
+                                     @
+        @ @@    @@@   @@@    @ @@   @@@@@
+        @@  @  @   @    @    @@  @   @
+        @   @  @   @    @    @   @   @  @
+        @@@@    @@@   @@@@@  @   @    @@
+        @
+        @
+    
+    """)
+    click.echo("Welcome to the Trustpoint Client Certificate Manager (tp-crt-mgr) v" + version +"!\n")
+    3+3
 
 @click.group()
 def cli() -> None:
-    """Trustpoint-Client program."""
-    pass
+    asciiLogo
 
 
 @cli.command()
 @click.option('--otp', '-o', required=True, type=str, help='The OTP for provisioning.')
 @click.option('--salt', '-s', required=True, type=str, help='The salt for provisioning.')
 @click.option('--url', '-u', required=True, type=str, help='The url endpoint for provisioning.')
-def provision(otp: str, salt: str, url: str) -> None:
+@click.option('--tpurl', '-t', required=False, type=str, help='The IP or domain address of the Trustpoint.')
+@click.option('--uriext', '-e', required=False, type=str, help='The uri extension for obtaining the trust store.')
+@click.option('--hexpass', '-p', required=False, type=str, help='The OTP for deriving the trust store key.')
+@click.option('--hexsalt', '-z', required=False, type=str, help='The salt for deriving the trust store key.') # TODO: Analyze if OK to re-use or derive from main salt
+def provision(otp: str, salt: str, url: str, tpurl :str ="127.0.0.1:5000", uriext: str ="", hexpass: str="", hexsalt: str="") -> None:
     """Provisions the Trustpoint-Client software."""
     try:
-        _provision(otp, salt, url)
+        _provision(otp, salt, url, tpurl, uriext, hexpass, hexsalt) # TODO: Quite a lot of parameters. Maybe better to pass an options list?
     except Exception as e:
         click.echo(f'Failed to provision the Trustpoint-Client.\n{e}')
         return
     click.echo('Successfully provisioned the Trustpoint-Client.')
 
+@cli.command()
+def logo() -> None:
+    click.echo('The subcommand')
+    drawAsciiLogo()
 
 if __name__ == '__main__':
     cli()
--- trustpoint_client/trustpoint_client.py
+++ trustpoint_client/trustpoint_client.py
@@ -1,3 +1,65 @@
-def provision(otp: str, salt: str, url: str) -> None:
+import click
+from os.path import exists
+import requests
+import hashlib
+import hmac
+
+def getTrustStore(tpurl :str ="127.0.0.1:5000", uriext: str ="", hexpass: str="", hexsalt: str="") -> bool:
+    click.echo('Retrieving Trustpoint Trust Store')
+    if exists("trust-store.pem") : # TODO it might be a security risk to have this (write) accessible on the filesystem
+        click.echo('trust-store.pem file present locally')
+        return True
+    
+    # Truststore file not present, obtain it
+    click.echo('trust-store.pem missing, downloading from Trustpoint...')
+    response = requests.get('https://' + tpurl + '/trust-point/rest/provision/trust-store/' + uriext, verify=False)
+
+    # DEBUG USE ONLY!!!
+    # response.headers["hmac-signature"] = "fe411166f41436e81edd3303f74858979f01a993f98998891f4f8c514f0fe7b8" # (for content "It's a Truststore baby." with pass "abc" and salt "def")
+
+    # TRUSTSTORE DOWNLOAD VERIFICATION
+    verificationLevel: int = 0
+    # Only allow for basic hash comparision verification if HMAC header is NOT included by Trustpoint AND hexpass is not provided
+    if hexpass or "hmac-signature" in response.headers: verificationLevel = 1
+
+    if verificationLevel == 1:
+        # Full HMAC + PBKDF2 verification
+        click.echo("Using PBKDF2-HMAC verification")
+        if not "hmac-signature" in response.headers: raise Exception("HMAC signature header is required but missing in Trustpoint response headers.")
+        if not hexpass: raise Exception("HMAC verification is required but --hexpass option was not provided.")
+        pbkdf2_iter = 1000000 # TODO this should be an option or given by a trustpoint header with a reasonable minimum sanity check
+        key = hashlib.pbkdf2_hmac('sha256', bytes(hexpass,'utf-8'), bytes(hexsalt,'utf-8'), pbkdf2_iter, dklen=32)
+        h = hmac.new(key, response.content, hashlib.sha256)
+        #click.echo(key.hex())
+        click.echo("Computed HMAC: " + h.hexdigest())
+        if not hmac.compare_digest(h.hexdigest(), response.headers["hmac-signature"]): raise Exception("Truststore HMAC signature header and computed HMAC do not match.")
+    elif verificationLevel == 0:
+        # Simple SHA3 hash display
+        click.echo("Using simple hash verification")
+        s = hashlib.sha3_256()
+        s.update(response.content)
+        hash = s.hexdigest()
+        explicitVerificationBytes = 3 # how many bytes of the hash the admin has to explicitely enter for checking, TODO this should be determined by trustpoint
+        if explicitVerificationBytes > 0:
+            value = click.prompt("Please enter the first " + str(explicitVerificationBytes*2) + " characters of the trust store hash displayed by Trustpoint")
+            if value != hash[:explicitVerificationBytes*2]: raise Exception("Downloaded and entered hash portion do not match.")
+        click.echo("\nPLEASE VERIFY BELOW HASH AGAINST THAT DISPLAYED BY TRUSTPOINT\n")
+        click.echo(hash)
+        feedback = click.prompt("\nDo the hashes match EXACTLY? (y/n)")
+        if feedback != 'Y' and feedback != 'y': raise Exception("Hash does not match the one displayed by Trustpoint, aborting.")
+    else:
+        raise Exception("Invalid verification level.")
+    
+    with open('trust-store.pem', 'wb') as f: # write downloaded truststore to FS
+        f.write(response.content)
+    
+    click.echo('Thank you, the trust store was downloaded successfully.')
+    return True
+
+def provision(otp: str, salt: str, url: str, tpurl :str, uriext: str, hexpass: str, hexsalt: str) -> None:
     """Provisions the Trustpoint-Client software."""
+    click.echo('Provisioning client...')
+    # Step 1: Get trustpoint Trust Store
+    res = getTrustStore(tpurl, uriext, hexpass, hexsalt)
+    print(res)
     pass
