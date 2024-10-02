d51e07898a4b6072b50336b8e66df802150691e9
Christian Schwinne <cschwinne@gmail.com>
2024-10-02 11:16:56 +0200

    Add platformdirs dependency and purge_init flag

--- trustpoint_client/cli/purge.py
+++ trustpoint_client/cli/purge.py
@@ -11,7 +11,7 @@ def purge() -> None:
     if click.confirm(
             '\n\tAre you sure you want to purge the Trustpoint Client?\n'
             '\tThis will remove all data, including all LDevID objects.\n\n'):
-        TrustpointClient().purge()
+        TrustpointClient(purge_init=True).purge()
         click.echo('\n\tSuccessfully purged the Trustpoint Client.\n')
     else:
         click.echo('\n\tAborted.\n')
