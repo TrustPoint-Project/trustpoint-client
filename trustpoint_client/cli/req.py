ea4e3f9b8584206af2e15dae822092cb1f9dd3dd
AlexHx8472 <alex_mail@posteo.de>
2024-10-23 16:18:19 +0200

    req wip

--- trustpoint_client/cli/req.py
+++ trustpoint_client/cli/req.py
@@ -1,8 +1,11 @@
-import datetime
-import uuid
+from __future__ import annotations
 
+import uuid
+import subprocess
 import click
 from pathlib import Path
+
+from cryptography.hazmat.primitives import serialization
 
 from trustpoint_client.api import TrustpointClient
 from trustpoint_client.api.schema import PkiProtocol
@@ -98,41 +101,51 @@ Extension Options:
 @click.option('--extension', '-e', type=str, required=False)
 def req_generic_cert(subject: list[str], extension: list[str]) -> None:
     """Request a new generic certificate."""
-    # trustpoint_client = TrustpointClient()
-    # if trustpoint_client.config.default_pki_protocol == PkiProtocol.CMP:
-    #     _reg_cmp_cert(trustpoint_client=trustpoint_client, subject=subject, extension=extension)
+    trustpoint_client = TrustpointClient()
+    if trustpoint_client.config.default_pki_protocol == PkiProtocol.CMP:
+        _reg_cmp_cert(trustpoint_client=trustpoint_client, subject=subject, extension=extension)
 
-# def _reg_cmp_cert(trustpoint_client: TrustpointClient, subject: list[str], extension: list[str]) -> None:
+def _reg_cmp_cert(trustpoint_client: TrustpointClient, subject: list[str], extension: list[str]) -> None:
 
-    # inventory_domain = trustpoint_client.inventory.domains[trustpoint_client.default_domain]
-    # key_index = inventory_domain.ldevid_credential.key_index
-    # cert_index = inventory_domain.ldevid_credential.active_certificate_index
-    #
-    # key = trustpoint_client.devid_module.inventory.devid_keys[key_index].private_key
-    # cert = trustpoint_client.devid_module.inventory.devid_certificates[cert_index].certificate
-    #
-    # key_path = trustpoint_client.inventory_file_path.parent / 'key.pem'
-    # cert_path = trustpoint_client.inventory_file_path.parent / 'cert.pem'
-    #
-    # key_path.write_bytes(key)
-    # cert_path.write_bytes(cert)
-    #
-    # new_key_path = trustpoint_client.inventory_file_path.parent / 'new_key_path.pem'
-    # new_cert_path = trustpoint_client.inventory_file_path.parent / 'new_cert_path.pem'
-    #
-    # cmd = (
-    #     f'openssl cmp '
-    #     f'-cmd ir '
-    #     f'-server {trustpoint_client.trustpoint_ipv4}:{trustpoint_client.trustpoint_port} '
-    #     f'-path /.well-known/cmp/p/{trustpoint_client.default_domain}/initialization/ '
-    #     f'-newkey {new_key_path} '
-    #     f'-key {key_path} '
-    #     f'-cert {cert_path} '
-    #     f'-certout {new_cert_path} '
-    #     f'-implicit_confirm -disable_confirm '
-    #     f'-unprotected_errors'
-    # )
+    inventory_domain = trustpoint_client.inventory.domains[trustpoint_client.default_domain]
+    key_index = inventory_domain.ldevid_credential.key_index
+    cert_index = inventory_domain.ldevid_credential.active_certificate_index
 
+    key = trustpoint_client.devid_module.inventory.devid_keys[key_index].private_key
+    cert = trustpoint_client.devid_module.inventory.devid_certificates[cert_index].certificate
+
+    key_path = trustpoint_client.inventory_file_path.parent / 'key.pem'
+    cert_path = trustpoint_client.inventory_file_path.parent / 'cert.pem'
+
+    key_path.write_bytes(key)
+    cert_path.write_bytes(cert)
+
+    new_key_path = trustpoint_client.inventory_file_path.parent / 'new_key_path.pem'
+    new_cert_path = trustpoint_client.inventory_file_path.parent / 'new_cert_path.pem'
+
+    new_private_key = trustpoint_client.generate_new_key(inventory_domain.signature_suite)
+    new_key_path.write_bytes(
+        new_private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+    ))
+
+    cmd = (
+        f'openssl cmp '
+        f'-cmd ir '
+        f'-server {trustpoint_client.trustpoint_ipv4}:{trustpoint_client.trustpoint_port} '
+        f'-path /.well-known/cmp/p/{trustpoint_client.default_domain}/initialization/ '
+        f'-newkey {new_key_path} '
+        f'-key {key_path} '
+        f'-cert {cert_path} '
+        f'-certout {new_cert_path} '
+        f'-implicit_confirm -disable_confirm '
+        f'-unprotected_errors'
+    )
+
+    result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    print(result)
 
 #
 # @req.command(name='tls-client-cert')
