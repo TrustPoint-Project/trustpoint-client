3732d519babe34442b379466740ccab9c2ec33ef
AlexHx8472 <alex_mail@posteo.de>
2024-11-11 13:51:20 +0100

    ruff formating and fixes cli.provision

--- trustpoint_client/cli/provision.py
+++ trustpoint_client/cli/provision.py
@@ -1,23 +1,37 @@
 """Command-line interface for the Trustpoint-Client."""
+
 from __future__ import annotations
+
+from pathlib import Path
 
 import click
 import prettytable
-from pathlib import Path
-
 from trustpoint_devid_module.serializer import CredentialSerializer
 
-from trustpoint_client.api.mdns import find_services
-from trustpoint_client.api.zero_touch_aoki import aoki_onboarding
 from trustpoint_client.api import TrustpointClient
+from trustpoint_client.api.mdns import find_services
 from trustpoint_client.api.schema import PkiProtocol
+from trustpoint_client.api.zero_touch_aoki import aoki_onboarding
 
 
+def pretty_print_provisioning_results(data: dict[str, str]) -> None:
+    """Echoes the results as PrettyTable to CLI (stdout).
+
+    Args:
+        data: Provisioning results.
+    """
+    click.echo('\nTrustpoint Client successfully provisioned.\n')
+    table = prettytable.PrettyTable(['Property', 'Value'])
+    table.add_rows([[key.capitalize(), value] for key, value in data.items()])
+    table.align = 'l'
+    click.echo(table)
+    click.echo()
 
 
 @click.group
 def provision() -> None:
     """Commands to provision the Trustpoint Client."""
+
 
 @provision.command
 @click.option('--otp', '-o', required=True, type=str, help='The One-Time Password to use.')
@@ -39,85 +53,77 @@ def auto(otp: str, device: str, host: str, port: int) -> None:
     except Exception as exception:
         raise click.ClickException(str(exception)) from exception
 
-    click.echo('\nTrustpoint Client successfully provisioned.\n')
-    table = prettytable.PrettyTable(['Property', 'Value'])
-    table.add_rows([[key.capitalize(), value] for key, value in result.items()])
-    table.align = 'l'
-    click.echo(table)
-    click.echo()
+    pretty_print_provisioning_results(result)
 
 
 @provision.command
+@click.option('--host', '-h', type=str, required=True, prompt=True, help='The trustpoint host name.')
+@click.option('--port', '-p', type=int, required=True, prompt=True, help='The trustpoint port.')
 @click.option(
-    '--trustpoint-host', '-th',
-    type=str,
-    required=True, prompt=True,
-    help='The trustpoint host name.')
-@click.option(
-    '--trustpoint-port', '-tp',
-    type=int,
-    required=True, prompt=True,
-    help='The trustpoint port.')
-@click.option(
-    '--pki-protocol', '-prot',
+    '--pki-protocol',
+    '-pki',
     type=click.Choice([pki_protocol.value for pki_protocol in PkiProtocol]),
-    required=True, prompt=True,
-    help='The pki protocol.')
+    required=True,
+    prompt=True,
+    help='The pki protocol.',
+)
 @click.option(
-    '--domain-credential-pkcs12', '-pkcs12',
+    '--domain-credential-pkcs12',
+    '-pkcs12',
     type=click.Path(),
     default=None,
-    help='PKCS#12 file path containing the full credential.')
+    help='PKCS#12 file path containing the full credential.',
+)
 @click.option(
-    '--domain-credential-certificate', '-cert',
+    '--domain-credential-certificate',
+    '-cert',
     type=click.Path(),
     default=None,
-    help='Certificate file path (PEM format)')
+    help='Certificate file path (PEM format)',
+)
 @click.option(
-    '--domain-credential-certificate-chain', '-chain',
+    '--domain-credential-certificate-chain',
+    '-chain',
     type=click.Path(),
     default=None,
-    help='Certificate chain file path (PEM format).')
+    help='Certificate chain file path (PEM format).',
+)
 @click.option(
-    '--domain-credential-private-key', '-key',
+    '--domain-credential-private-key',
+    '-key',
     type=click.Path(),
     default=None,
-    help='Private key file path (PEM format).')
+    help='Private key file path (PEM format).',
+)
 @click.option(
-    '--password', '-pw',
-    type=str,
-    default=None,
-    help='The password to decrypt the private key / PKCS#12 file.')
-def manual(
-        trustpoint_host: str,
-        trustpoint_port: int,
-        pki_protocol: str,
-        domain_credential_pkcs12: None | str,
-        domain_credential_certificate: None | str,
-        domain_credential_certificate_chain: None | str,
-        domain_credential_private_key: None | str,
-        password: None | str) -> None:
+    '--password', '-pw', type=str, default=None, help='The password to decrypt the private key / PKCS#12 file.'
+)
+def manual(     # noqa: PLR0913, C901
+    host: str,
+    port: int,
+    pki_protocol: str,
+    domain_credential_pkcs12: None | str,
+    domain_credential_certificate: None | str,
+    domain_credential_certificate_chain: None | str,
+    domain_credential_private_key: None | str,
+    password: None | str,
+) -> None:
     """Creates a domain and injects the domain credential from file."""
     trustpoint_client = TrustpointClient()
-    #
-    # host = None
-    # port = None
-    # if ':' in trustpoint_host:
-    #     host, port = trustpoint_host.split(':')
-    #     print('nope')
-    # elif not trustpoint_port:
-    #     port = 443
-    # if not host or not port:
-    #     raise click.ClickException('Please provide a valid host and port.')
-    # port = int(port)
+
+    if not port:
+        port = 443
+    if ':' in host:
+        host, port = host.split(':')
+    port = int(port)
 
     if domain_credential_pkcs12:
         domain_credential_pkcs12 = Path(domain_credential_pkcs12).read_bytes()
         try:
             credential = CredentialSerializer(domain_credential_pkcs12, password=password)
         except Exception as exception:
-            raise click.ClickException(
-                'Failed to parse the PKCS#12 file. Either malformed or wrong password.') from exception
+            err_msg = 'Failed to parse the PKCS#12 file. Either malformed or wrong password.'
+            raise click.ClickException(err_msg) from exception
     else:
         if domain_credential_certificate:
             domain_credential_certificate = Path(domain_credential_certificate).read_bytes()
@@ -129,45 +135,44 @@ def manual(
             password = password.encode()
         try:
             credential = CredentialSerializer(
-                (
-                    domain_credential_private_key,
-                    domain_credential_certificate,
-                    domain_credential_certificate_chain
-                ),
-                password=password
+                (domain_credential_private_key, domain_credential_certificate, domain_credential_certificate_chain),
+                password=password,
             )
         except Exception as exception:
-            raise click.ClickException(
-                'Failed to parse given credential. Either malformed or wrong password.') from exception
+            err_msg = 'Failed to parse given credential. Either malformed or wrong password.'
+            raise click.ClickException(err_msg) from exception
 
-    # try:
-    pki_protocol = PkiProtocol(pki_protocol.upper())
-    result = trustpoint_client.provision_manual(
-        trustpoint_host=trustpoint_host,
-        trustpoint_port=trustpoint_port,
-        pki_protocol=pki_protocol,
-        credential=credential
-    )
-    # except Exception as exception:
-    #     raise click.ClickException(str(exception)) from exception
+    try:
+        pki_protocol = PkiProtocol(pki_protocol.upper())
+        result = trustpoint_client.provision_manual(
+            trustpoint_host=host, trustpoint_port=port, pki_protocol=pki_protocol, credential=credential
+        )
+    except Exception as exception:
+        raise click.ClickException(str(exception)) from exception
 
-    click.echo('\nTrustpoint Client successfully provisioned.\n')
-    table = prettytable.PrettyTable(['Property', 'Value'])
-    table.add_rows([[key.capitalize(), value] for key, value in result.items()])
-    table.align = 'l'
-    click.echo(table)
-    click.echo()
+    pretty_print_provisioning_results(result)
 
 
 @provision.command
-@click.option('--host', '-h', required=False, type=str,
-              help='The domain or IP address of the Trustpoint. For testing without discovery.')
-@click.option('--port', '-p', required=False, type=int, default=443,
-              help='The port of the Trustpoint if not 443. For testing without discovery.')
-def zero_touch(host: str, port: int):
+@click.option(
+    '--host',
+    '-h',
+    required=False,
+    type=str,
+    help='The domain or IP address of the Trustpoint. For testing without discovery.',
+)
+@click.option(
+    '--port',
+    '-p',
+    required=False,
+    type=int,
+    default=443,
+    help='The port of the Trustpoint if not 443. For testing without discovery.',
+)
+def zero_touch(host: str, port: int) -> None:
     """Starts the AOKI demo zero-touch onboarding process."""
-
-    if host is None:  # regular zero-touch onboarding with mDNS discovery
+    # regular zero-touch onboarding with mDNS discovery
+    if host is None:
         find_services(zero_touch=True)
         return
 
