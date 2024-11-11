3dfa5ca45504d0427b99a69c9a99ea100117c420
AlexHx8472 <alex_mail@posteo.de>
2024-11-11 13:59:10 +0100

    ruff format & check fixes cli.version

--- trustpoint_client/cli/version.py
+++ trustpoint_client/cli/version.py
@@ -1,14 +1,17 @@
+"""Trustpoint-Client commands concerning its version."""
 from __future__ import annotations
 
-import click
 import importlib.metadata
+
+import click
 
 try:
     version_id = importlib.metadata.version('trustpoint_client')
 except Exception as exception:
     raise click.ClickException(str(exception)) from exception
 
+
 @click.command
-def version():
+def version() -> None:
     """Displays the version of Trustpoint-Client."""
     click.echo(f'\n\tTrustpoint Client Version: v{version_id}\n')
