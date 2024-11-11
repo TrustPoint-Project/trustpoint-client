480cf8bb4b9db5db6c6de64848674d9ce355e5a3
AlexHx8472 <alex_mail@posteo.de>
2024-11-11 14:15:24 +0100

    excption handling cli.purge

--- trustpoint_client/cli/purge.py
+++ trustpoint_client/cli/purge.py
@@ -14,7 +14,10 @@ def purge() -> None:
         '\n\tAre you sure you want to purge the Trustpoint Client?\n'
         '\tThis will remove all data, including all LDevID objects.\n\n'
     ):
-        purge_working_dir_inventory_and_config()
-        click.echo('\n\tSuccessfully purged the Trustpoint Client.\n')
+        try:
+            purge_working_dir_inventory_and_config()
+            click.echo('\n\tSuccessfully purged the Trustpoint Client.\n')
+        except Exception as exception:
+            raise click.ClickException(str(exception)) from exception
     else:
         click.echo('\n\tAborted.\n')
