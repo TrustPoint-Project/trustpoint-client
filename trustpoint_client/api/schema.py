33b5b43e627ec475bc1e38d5d36d05bb3f7e20ef
AlexHx8472 <alex_mail@posteo.de>
2025-01-31 15:03:10 +0100

    api schema refactored, ruff & mypy passed

--- trustpoint_client/api/schema.py
+++ trustpoint_client/api/schema.py
@@ -1,12 +1,20 @@
+"""Defines the pydantic models used for persistent data storage."""
+
 from __future__ import annotations
 
+import datetime  # noqa: TCH003
 import enum
+
+from cryptography.hazmat.primitives.asymmetric import ec, rsa
 from pydantic import BaseModel, ConfigDict
-import datetime
-from cryptography.hazmat.primitives.asymmetric import rsa, ec
+
+KEY_2K = 2048
+KEY_3K = 3072
+KEY_4K = 4096
 
 
 class SignatureSuite(enum.Enum):
+    """Supported signature suites."""
 
     RSA2048 = 'RSA2048SHA256'
     RSA3072 = 'RSA3072SHA256'
@@ -16,13 +24,22 @@ class SignatureSuite(enum.Enum):
 
     @classmethod
     def get_signature_suite_by_public_key(
-            cls, public_key: rsa.RSAPublicKey | ec.EllipticCurvePublicKey) -> SignatureSuite:
+        cls, public_key: rsa.RSAPublicKey | ec.EllipticCurvePublicKey
+    ) -> SignatureSuite:
+        """Gets the matching signature suite for the given public key.
+
+        Args:
+            public_key: The public key to get the signature suite for.
+
+        Returns:
+            SignatureSuite: The matching signature suite for the given public key.
+        """
         if isinstance(public_key, rsa.RSAPublicKey):
-            if public_key.key_size == 2048:
+            if public_key.key_size == KEY_2K:
                 return cls.RSA2048
-            if public_key.key_size == 3072:
+            if public_key.key_size == KEY_3K:
                 return cls.RSA3072
-            if public_key.key_size == 4096:
+            if public_key.key_size == KEY_4K:
                 return cls.RSA4096
             raise ValueError
 
@@ -37,6 +54,7 @@ class SignatureSuite(enum.Enum):
 
 
 class PkiProtocol(enum.Enum):
+    """Supported PKI-protocols."""
 
     CMP = 'CMP'
     EST = 'EST'
@@ -45,7 +63,9 @@ class PkiProtocol(enum.Enum):
 
 
 class CertificateType(enum.Enum):
+    """Supported certificate types (templates)."""
 
+    IDevID = 'IDevID'
     LDEVID = 'LDevID'
     GENERIC = 'Generic'
     TLS_CLIENT = 'TLS Client Certificate'
@@ -55,53 +75,51 @@ class CertificateType(enum.Enum):
     OPC_UA_CLIENT = 'OPC UA Client Certificate'
     OPC_UA_SERVER = 'OPC UA Server Certificate'
 
-class TrustpointClientConfigModel(BaseModel):
-    """The Trustpoint Client Configuration Schema."""
-    model_config = ConfigDict(strict=True, extra='allow')
-
-    default_domain: None | str = ...
-
 
 class DomainConfigModel(BaseModel):
     """The Domain Configuration Schema."""
+
     model_config = ConfigDict(strict=True, extra='forbid')
 
-    device: str = ...
-    serial_number: str = ...
-    domain: str = ...
-    trustpoint_host: None | str = ...
-    trustpoint_port: None | int = ...
-    signature_suite: SignatureSuite = ...
-    pki_protocol: PkiProtocol = ...
-    tls_trust_store: str = ...
+    trustpoint_addresses: list[str] = ...  # type: ignore[assignment]
+    signature_suite: SignatureSuite = ...  # type: ignore[assignment]
+    pki_protocol: PkiProtocol = ...  # type: ignore[assignment]
+    tls_trust_store: str = ...  # type: ignore[assignment]
 
 
 class CredentialModel(BaseModel):
     """The credential model."""
+
     model_config = ConfigDict(strict=True, extra='forbid')
 
-    unique_name: str = ...
-    certificate_index: int = ...
-    key_index: int = ...
-    subject: str = ...
-    certificate_type: CertificateType = ...
-    not_valid_before: datetime.datetime = ...
-    not_valid_after: datetime.datetime = ...
+    unique_name: str = ...  # type: ignore[assignment]
+    certificate_index: int = ...  # type: ignore[assignment]
+    key_index: int = ...  # type: ignore[assignment]
+    subject: str = ...  # type: ignore[assignment]
+    certificate_type: CertificateType = ...  # type: ignore[assignment]
+    not_valid_before: datetime.datetime = ...  # type: ignore[assignment]
+    not_valid_after: datetime.datetime = ...  # type: ignore[assignment]
 
 
 class DomainModel(BaseModel):
     """The domain model."""
+
     model_config = ConfigDict(strict=True, extra='forbid')
 
-    domain_config: DomainConfigModel = ...
+    domain_config: DomainConfigModel = ...  # type: ignore[assignment]
 
-    ldevid_credential: CredentialModel = ...
-    credentials: dict[str, CredentialModel] = ...
-    trust_stores: dict[str, list[str]] = ...
+    idevid_credential: None | CredentialModel = ...  # type: ignore[assignment]
+    ldevid_credential: CredentialModel = ...  # type: ignore[assignment]
+    credentials: dict[str, CredentialModel] = ...  # type: ignore[assignment]
+    trust_stores: dict[str, str] = ...  # type: ignore[assignment]
 
 
 class InventoryModel(BaseModel):
     """The inventory model."""
+
     model_config = ConfigDict(strict=True, extra='forbid')
 
-    domains: dict[str, DomainModel] = ...
+    default_domain: None | str = ...  # type: ignore[assignment]
+    device_serial_number: None | str = ...  # type: ignore[assignment]
+
+    domains: dict[str, DomainModel] = ...  # type: ignore[assignment]
