b8c1ee4e47fb6499b82e6a94500d66dd269e42c4
AlexHx8472 <alex_mail@posteo.de>
2025-02-05 10:43:21 +0100

    schema changes

--- trustpoint_client/schema.py
+++ trustpoint_client/schema.py
@@ -4,8 +4,10 @@ from __future__ import annotations
 
 import datetime  # noqa: TCH003
 import enum
-
-from cryptography.hazmat.primitives.asymmetric import ec, rsa
+# from ipaddress import IPv4Address
+#
+# from cryptography.hazmat.primitives.asymmetric import ec, rsa
+from trustpoint_client.oid import SignatureSuite
 from pydantic import BaseModel, ConfigDict
 
 KEY_2K = 2048
@@ -13,61 +15,52 @@ KEY_3K = 3072
 KEY_4K = 4096
 
 
-class SignatureSuite(enum.Enum):
-    """Supported signature suites."""
-
-    RSA2048 = 'RSA2048SHA256'
-    RSA3072 = 'RSA3072SHA256'
-    RSA4096 = 'RSA4096SHA256'
-    SECP256R1 = 'SECP256R1SHA256'
-    SECP384R1 = 'SECP384R1SHA384'
-
-    @classmethod
-    def get_signature_suite_by_public_key(
-        cls, public_key: rsa.RSAPublicKey | ec.EllipticCurvePublicKey
-    ) -> SignatureSuite:
-        """Gets the matching signature suite for the given public key.
-
-        Args:
-            public_key: The public key to get the signature suite for.
-
-        Returns:
-            SignatureSuite: The matching signature suite for the given public key.
-        """
-        if isinstance(public_key, rsa.RSAPublicKey):
-            if public_key.key_size == KEY_2K:
-                return cls.RSA2048
-            if public_key.key_size == KEY_3K:
-                return cls.RSA3072
-            if public_key.key_size == KEY_4K:
-                return cls.RSA4096
-            raise ValueError
-
-        if isinstance(public_key, ec.EllipticCurvePublicKey):
-            if isinstance(public_key.curve, ec.SECP256R1):
-                return cls.SECP256R1
-            if isinstance(public_key.curve, ec.SECP384R1):
-                return cls.SECP384R1
-            raise ValueError
-
-        raise ValueError
-
-
-class PkiProtocol(enum.Enum):
-    """Supported PKI-protocols."""
-
-    CMP = 'CMP'
-    EST = 'EST'
-    SCEP = 'SCEP'
-    REST = 'REST'
+# class SignatureSuite(enum.Enum):
+#     """Supported signature suites."""
+#
+#     RSA2048 = 'RSA2048SHA256'
+#     RSA3072 = 'RSA3072SHA256'
+#     RSA4096 = 'RSA4096SHA256'
+#     SECP256R1 = 'SECP256R1SHA256'
+#     SECP384R1 = 'SECP384R1SHA384'
+#
+#     @classmethod
+#     def get_signature_suite_by_public_key(
+#         cls, public_key: rsa.RSAPublicKey | ec.EllipticCurvePublicKey
+#     ) -> SignatureSuite:
+#         """Gets the matching signature suite for the given public key.
+#
+#         Args:
+#             public_key: The public key to get the signature suite for.
+#
+#         Returns:
+#             SignatureSuite: The matching signature suite for the given public key.
+#         """
+#         if isinstance(public_key, rsa.RSAPublicKey):
+#             if public_key.key_size == KEY_2K:
+#                 return cls.RSA2048
+#             if public_key.key_size == KEY_3K:
+#                 return cls.RSA3072
+#             if public_key.key_size == KEY_4K:
+#                 return cls.RSA4096
+#             raise ValueError
+#
+#         if isinstance(public_key, ec.EllipticCurvePublicKey):
+#             if isinstance(public_key.curve, ec.SECP256R1):
+#                 return cls.SECP256R1
+#             if isinstance(public_key.curve, ec.SECP384R1):
+#                 return cls.SECP384R1
+#             raise ValueError
+#
+#         raise ValueError
 
 
 class CertificateType(enum.Enum):
     """Supported certificate types (templates)."""
 
-    IDevID = 'IDevID'
-    LDEVID = 'LDevID'
-    GENERIC = 'Generic'
+    IDevID = 'IDevID Certificate'
+    DOMAIN = 'Domain Credential Certificate'
+    GENERIC = 'Generic Certificate'
     TLS_CLIENT = 'TLS Client Certificate'
     TLS_SERVER = 'TLS Server Certificate'
     MQTT_CLIENT = 'MQTT Client Certificate'
@@ -82,9 +75,8 @@ class DomainConfigModel(BaseModel):
     model_config = ConfigDict(strict=True, extra='forbid')
 
     trustpoint_addresses: list[str] = ...  # type: ignore[assignment]
-    signature_suite: SignatureSuite = ...  # type: ignore[assignment]
-    pki_protocol: PkiProtocol = ...  # type: ignore[assignment]
-    tls_trust_store: str = ...  # type: ignore[assignment]
+    # signature_suite: SignatureSuite = ...  # type: ignore[assignment]
+    tls_trust_store: list[str] = ...  # type: ignore[assignment]
 
 
 class CredentialModel(BaseModel):
@@ -92,14 +84,12 @@ class CredentialModel(BaseModel):
 
     model_config = ConfigDict(strict=True, extra='forbid')
 
-    unique_name: str = ...  # type: ignore[assignment]
     certificate_index: int = ...  # type: ignore[assignment]
     key_index: int = ...  # type: ignore[assignment]
     subject: str = ...  # type: ignore[assignment]
     certificate_type: CertificateType = ...  # type: ignore[assignment]
     not_valid_before: datetime.datetime = ...  # type: ignore[assignment]
     not_valid_after: datetime.datetime = ...  # type: ignore[assignment]
-
 
 class DomainModel(BaseModel):
     """The domain model."""
@@ -109,7 +99,7 @@ class DomainModel(BaseModel):
     domain_config: DomainConfigModel = ...  # type: ignore[assignment]
 
     idevid_credential: None | CredentialModel = ...  # type: ignore[assignment]
-    ldevid_credential: CredentialModel = ...  # type: ignore[assignment]
+    domain_credential: CredentialModel = ...  # type: ignore[assignment]
     credentials: dict[str, CredentialModel] = ...  # type: ignore[assignment]
     trust_stores: dict[str, str] = ...  # type: ignore[assignment]
 
@@ -123,3 +113,31 @@ class InventoryModel(BaseModel):
     device_serial_number: None | str = ...  # type: ignore[assignment]
 
     domains: dict[str, DomainModel] = ...  # type: ignore[assignment]
+
+
+class IdevidCredentialModel(BaseModel):
+    """The IDevID Credential Model."""
+
+    model_config = ConfigDict(strict=True, extra='forbid')
+
+    private_key: str = ...  # type: ignore[assignment]
+    certificate: str = ...  # type: ignore[assignment]
+
+
+class IdevidHierarchyModel(BaseModel):
+    """The IDevID Hierarchy Model."""
+
+    model_config = ConfigDict(strict=True, extra='forbid')
+
+    root_ca_certificate: str = ...  # type: ignore[assignment]
+    issuing_ca_certificate: str = ...   # type: ignore[assignment]
+
+    issued_idevids: dict[str, IdevidCredentialModel] = ...  # type: ignore[assignment]
+
+
+class IdevidModel(BaseModel):
+    """The IDevID model."""
+
+    model_config = ConfigDict(strict=True, extra='forbid')
+
+    hierarchies: dict[str, IdevidHierarchyModel] = ...   # type: ignore[assignment]
